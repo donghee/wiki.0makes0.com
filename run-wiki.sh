7da2622e018e3f8ef9dee1d14b160b21c473b88d
시민 <dongheepark@gmail.com>
2021-10-15 01:16:38 +0900

    tailwindcss

--- run-wiki.sh
+++ run-wiki.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-npx tailwindcss -i theme/css/tailwind.css -o uploads/tailwind.css -c tailwind.config.js
+npx tailwindcss -i theme/css/tailwind.css -o tailwind.css -c tailwind.config.js
 
 gollum --allow-uploads=dir --template-dir=templates --h1-title  --css --adapter rugged
