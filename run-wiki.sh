a734b026fd481012a8af2636b1ac6ad68768f0eb
Donghee <dongheepark@gmail.com>
2025-01-25 15:53:18 +0900

    Not allow edit

--- run-wiki.sh
+++ run-wiki.sh
@@ -2,4 +2,5 @@
 
 npx tailwindcss -i theme/css/tailwind.css -o tailwind.css -c tailwind.config.js
 
-gollum --allow-uploads=dir --template-dir=templates --h1-title  --css --adapter rugged
+gollum --allow-uploads=dir --template-dir=templates --h1-title  --css --adapter rugged --no-edit
+
